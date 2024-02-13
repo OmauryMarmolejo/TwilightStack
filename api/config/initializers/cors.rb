ca774d76986852f10d0d04a069f811fad2315c0c
Omaury Marmolejo <omaury@tango.io>
2024-02-13 16:54:44 -0600

    Allow any origin

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com"
+    # "*" allows requests from any origin, be careful with this setting
+    origins "*"
 
     resource "*",
       headers: :any,
